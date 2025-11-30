20abc2349dffea9d2183282e87808aa59e1db186
Rafael Beltran <rafabeltrans17@gmail.com>
2025-11-29 22:04:52 -0600

    fix export table typo

--- src/lib/exporTableToXlsx.ts
+++ src/lib/exporTableToXlsx.ts
@@ -1,10 +1,11 @@
 import * as XLSX from 'xlsx';
 import { saveAs } from 'file-saver';
+import { Table } from '@tanstack/react-table';
 
-export const exportTableToXlsx = (table, sheetName, fileName) => {
+export const exportTableToXlsx = <TData>(table: Table<TData>, sheetName: string, fileName: string) => {
     const wb = XLSX.utils.book_new();
     const ws = XLSX.utils.json_to_sheet(
-        table.getCoreRowModel().flatRows.map(row => row.original)
+        table.getCoreRowModel().flatRows.map((row) => row.original)
     );
 
     XLSX.utils.book_append_sheet(wb, ws, sheetName);
